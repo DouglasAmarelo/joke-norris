92eccdd0e582729fb3eda8771425508ffe8f7418
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-10-21 22:20:38 -0300

    :nail_care: Criação do estilo global da aplicação

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import GlobalStyles from './components/styles/global';
+import GlobalStyles from './styles/global';
 
 function App() {
 	return (
