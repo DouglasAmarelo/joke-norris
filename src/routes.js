39e4942778bf33326f68b5ed5e2b75199630daf2
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-10-21 22:09:26 -0300

    :seedling: Arquivo de controle das ramificações de páginas

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Joke from './pages/Joke';
+import NotFound from './pages/NotFound';
+
+const Routes = () => {
+	return(
+		<Switch>
+			<Route path="/" exact component={Home} />
+			<Route path="/category/:category/:joke" component={Joke} />
+			<Route component={NotFound} />
+		</Switch>
+	);
+};
+
+export default Routes;
