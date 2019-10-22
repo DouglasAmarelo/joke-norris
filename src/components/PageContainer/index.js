6741ca913f81353085e6b0b152b2ee32d2f0ff74
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-10-22 20:25:40 -0300

    :page_facing_up: Criação do componente que envolve toda a página

---
+++ src/components/PageContainer/index.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+import * as S from './styles';
+
+const PageContainer = ({ children }) => {
+	const [bgColor, setBgColor] = useState({ colorFrom: '#AA076B', colorTo: '#61045F', });
+
+	useEffect(() => {
+		const pageColors = [
+			{ colorFrom: '#02AAB0', colorTo: '#00CDAC', },
+			{ colorFrom: '#1A2980', colorTo: '#26D0CE', },
+			{ colorFrom: '#1A2980', colorTo: '#61045F', },
+			{ colorFrom: '#AA076B', colorTo: '#61045F', },
+			{ colorFrom: '#AA076B', colorTo: '#EA384D', },
+			{ colorFrom: '#D31027', colorTo: '#EA384D', },
+			{ colorFrom: '#E52D27', colorTo: '#B31217', },
+		];
+
+		const chooseRandomColor = (arrColors) => {
+			const randomItem = Math.floor(Math.random() * arrColors.length);
+			setBgColor(arrColors[randomItem]);
+		};
+
+		chooseRandomColor(pageColors);
+	}, []);
+
+
+	return(
+		<S.PageContainer bgColors={bgColor}>
+			<S.Content>
+				{children}
+			</S.Content>
+		</S.PageContainer>
+	);
+};
+
+export default PageContainer;
---
+++ src/components/PageContainer/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.section`
+	/* background: #1A2980; */
+	background: ${props => (
+		props.bgColors ? `linear-gradient(to right, ${props.bgColors.colorFrom}, ${props.bgColors.colorTo})` : `linear-gradient(to right, #d0d, #dd0)`
+	)};
+	display: flex;
+	flex-direction: column;
+	min-height: 100%;
+	justify-content: center;
+	transition: background 2s ease;
+`;
+
+export const Content = styled.div`
+	margin: 0 auto;
+	max-width: 1400px;
+	padding: 30px;
+`;
